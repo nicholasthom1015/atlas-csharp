d3d5b27b4a8a6192cca3cec4bcdfb1bb7d6349ec
nicholasthom1015 <103206977+nicholasthom1015@users.noreply.github.com>
2024-01-22 13:37:55 -0600

    Create queue.cs

---
+++ csharp-generics/0-queue/queue.cs
@@ -0,0 +1,13 @@
+using System;
+
+/// <synaps> Queue class name </synaps>
+public class Queue<T>
+{
+
+    /// <synaps> Returns Queue's type </synaps>
+    public Type CheckType()
+    {
+        return typeof(T);
+    }
+
+}
