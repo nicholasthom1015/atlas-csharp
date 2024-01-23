3cca48ffeee8ba316d3dc280eefc9aaa59a1f94d
nicholasthom1015 <103206977+nicholasthom1015@users.noreply.github.com>
2024-01-23 13:48:40 -0600

    Create 2-validation.cs

---
+++ csharp-delegates_events/2-validation/2-validation.cs
@@ -0,0 +1,60 @@
+using System;
+
+
+/// <synaps> Player class </synaps>
+public class Player
+{
+    /// <synaps> Name of Player </synaps>
+    protected string name;
+  /// <synaps> Current hp of Player </synaps>
+    protected float hp;
+    /// <synaps> maxHp of Player </synaps>
+    protected float maxHp;
+
+    /// <synaps> Player Builder </synaps>
+    public Player(string name="Player", float maxHp=100f)
+    {
+        this.name = name;
+        if( maxHp <= 0f){
+            Console.WriteLine("maxHp must be greater than 0. maxHp set to 100f by default.");
+            maxHp = 100f;
+        }
+        this.maxHp = maxHp;
+        this.hp = this.maxHp;
+    }
+
+    /// <synaps> Display Name, Hp, maxHp </synaps>
+    public void PrintHealth()
+    {
+        Console.WriteLine("{0} has {1} / {2} health", name, hp, maxHp);
+    }
+
+    /// <synaps> Player is Healed </synaps>
+    public void HealDamage(float heal)
+    {
+        if( heal < 0f)
+            heal = 0f;
+       // Console.WriteLine("{0} uses Cure", name);
+        Console.WriteLine("{0} heals {1} HP!", name, heal);
+        ValidateHP(hp + heal);
+
+    }
+
+    /// <synaps> Player takes Damage </synaps>
+    public void TakeDamage(float damage)
+    {
+        if( damage < 0f)
+            damage = 0f;
+           // Console.WriteLine("{0} is going to feel that", name);
+            Console.WriteLine("{0} takes {1} damage!", name, damage);
+            ValidateHP(hp - damage);
+
+    }
+
+   /// <synaps> Validate Player Hp </synaps>
+    public void ValidateHP(float newHp)
+    {
+        hp = Math.Clamp(newHp, 0, maxHp);
+    }
+  
+}
