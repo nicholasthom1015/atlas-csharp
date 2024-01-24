284665eae4deabd3b9dd7c218d62932118c05769
nicholasthom1015 <103206977+nicholasthom1015@users.noreply.github.com>
2024-01-24 17:46:43 -0600

    Create BaseClass.cs

---
+++ csharp-text_based_interface/ InventoryLibrary/BaseClass.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace InventoryLibrary
+{
+    /// <synaps> BaseClass </synaps>
+    public class BaseClass
+    {
+        /// <synaps> Id of BaseClass </synaps>
+        public string id { get;}
+
+        /// <synaps> date created </synaps>
+        public DateTime date_created { get;}
+
+        /// <synaps> date updated </synaps>
+        public DateTime date_updated { get;}
+    
+        /// <synaps> BaseClass Builder </synaps>
+        public BaseClass()
+        {
+            this.id = Guid.NewGuid().ToString();
+            DateTime curent = DateTime.Now;
+            this.date_created = curent;
+            this.date_updated = curent;
+        }
+            
+    }
+}
