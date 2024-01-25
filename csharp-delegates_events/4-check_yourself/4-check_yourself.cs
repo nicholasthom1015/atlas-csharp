a9dd1299761865cc77254b8ecf2a676b7182f30a
nicholasthom1015 <103206977+nicholasthom1015@users.noreply.github.com>
2024-01-25 00:08:21 -0600

    back to sq 1

--- csharp-delegates_events/4-check_yourself/4-check_yourself.cs
+++ csharp-delegates_events/4-check_yourself/4-check_yourself.cs
@@ -74,18 +74,18 @@ public class Player
     }
 
     private void HPValueWarning(object sender, CurrentHPArgs e)
-  //  {
-       // if ( e.currentHp == 0)
-       // {
-          //  Console.ForegroundColor = ConsoleColor.Red;
-       //     Console.WriteLine("Health has reached zero!");
-         //   Console.ResetColor();
-          //  return ;
-     //   }
-      //  Console.ForegroundColor = ConsoleColor.Yellow;
-     //   Console.WriteLine("Health is low!");
-     //   Console.ResetColor();
-  //  }
+    {
+        if ( e.currentHp == 0)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Health has reached zero!");
+            Console.ResetColor();
+            return ;
+        }
+        Console.ForegroundColor = ConsoleColor.Yellow;
+        Console.WriteLine("Health is low!");
+        Console.ResetColor();
+    }
 
     /// <synaps> ApplyModifier </synaps>
     public float ApplyModifier(float baseValue, Modifier modifier)
